ce468ee8e65f5f454c1fde3ae0583a4246089aab
Stefano Raffini <stefanoraffini@drwolf.it>
2011-07-01 13:45:43 +0200

    Corretto bug nell'autenticazione. Ora sembra funzionare con la nuova entity UserSession
    Signed-off-by: Stefano Raffini <stefanoraffini@drwolf.it>

--- force-ejb/ejbModule/it/drwolf/slot/session/Authenticator.java
+++ force-ejb/ejbModule/it/drwolf/slot/session/Authenticator.java
@@ -1,18 +1,9 @@
 package it.drwolf.slot.session;
 
-import it.drwolf.force.entity.Azienda;
 import it.drwolf.force.session.UserSession;
 import it.drwolf.slot.alfresco.AlfrescoInfo;
 import it.drwolf.slot.alfresco.AlfrescoUserIdentity;
-import it.drwolf.slot.alfresco.webscripts.AlfrescoWebScriptClient;
-import it.drwolf.slot.alfresco.webscripts.model.Authority;
-import it.drwolf.slot.alfresco.webscripts.model.AuthorityType;
-import it.drwolf.slot.entity.SlotInst;
 import it.drwolf.slot.entitymanager.PreferenceManager;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 import javax.persistence.EntityManager;
 
@@ -51,44 +42,6 @@ public class Authenticator {
 	@In
 	private UserSession userSession;
 
-	private Long slotDefId;
-
-	private Long slotInstId;
-
-	private void assignGroups() {
-		AlfrescoWebScriptClient alfrescoWebScriptClient = new AlfrescoWebScriptClient(
-				this.identity.getCredentials().getUsername(), this.identity
-						.getCredentials().getPassword(),
-				this.alfrescoInfo.getRepositoryUri());
-
-		List<Authority> userGroups = new ArrayList<Authority>();
-		List<Authority> groups = alfrescoWebScriptClient.getGroupsList("*", "");
-		for (Authority group : groups) {
-			List<Authority> users = alfrescoWebScriptClient
-					.getListOfChildAuthorities(group.getShortName(),
-							AuthorityType.USER.name());
-			Iterator<Authority> iterator = users.iterator();
-			boolean found = false;
-			while (iterator.hasNext() && (found == false)) {
-				Authority user = iterator.next();
-				if (user.getShortName().equals(
-						this.identity.getCredentials().getUsername())) {
-					userGroups.add(group);
-					found = true;
-				}
-			}
-		}
-		this.alfrescoUserIdentity.setGroups(userGroups);
-
-		// SETTO A MERDA il primo della lista come activeGroup
-		this.alfrescoUserIdentity.setActiveGroup(userGroups.get(0));
-		System.out.println("---> "
-				+ this.identity.getCredentials().getUsername()
-				+ " entered as \""
-				+ this.alfrescoUserIdentity.getActiveGroup().getShortName()
-				+ "\" member");
-	}
-
 	public boolean authenticate() {
 		this.log.info("authenticating {0}", this.credentials.getUsername());
 		// write your authentication logic here,
@@ -110,77 +63,17 @@ public class Authenticator {
 		if (this.alfrescoUserIdentity.authenticate(this.identity
 				.getCredentials().getUsername(), this.identity.getCredentials()
 				.getPassword(), this.alfrescoInfo.getRepositoryUri())) {
-			this.assignGroups();
-			if (this.alfrescoUserIdentity.isMemberOf(this.preferenceManager
-					.getPreference("FORCE_ADMIN").getStringValue())) {
-				this.identity.addRole("ADMIN");
-			} else {
-				AlfrescoWebScriptClient awsc = new AlfrescoWebScriptClient(
-						this.alfrescoInfo.getAdminUser(),
-						this.alfrescoInfo.getAdminPwd(),
-						this.alfrescoInfo.getRepositoryUri());
-				List<Authority> lista = awsc.getListOfChildAuthorities(
-						this.preferenceManager
-								.getPreference("FORCE_USER_GROUP")
-								.getStringValue(), "GROUP");
-				for (Authority authority : lista) {
-					if (authority.getShortName().equals(
-							this.alfrescoUserIdentity.getActiveGroup()
-									.getShortName())) {
-						this.identity.addRole("AZIENDE");
-						// devo prendere l'id dello slotdef associato
-						Azienda azienda = (Azienda) this.entityManager
-								.createQuery(
-										"from Azienda where emailReferente = :username")
-								.setParameter(
-										"username",
-										this.identity.getCredentials()
-												.getUsername())
-								.getSingleResult();
-						Long slotDefId = azienda.getSettore().getSlotDef()
-								.getId();
-						this.setSlotDefId(slotDefId);
-						SlotInst slonInst;
-						try {
-							slonInst = (SlotInst) this.entityManager
-									.createQuery(
-											"from SlotInst where slotDef = :slotDef and ownerId = :ownerId")
-									.setParameter("slotDef",
-											azienda.getSettore().getSlotDef())
-									.setParameter("ownerId",
-											azienda.getAlfrescoGroupId())
-									.getSingleResult();
-							if (slonInst != null) {
-								this.setSlotInstId(slonInst.getId());
-							}
-						} catch (Exception e) {
-							// Non è ancora stato creato uno slotInst
-						}
-						return true;
-					}
 
-				}
-
+			try {
+				this.userSession.init();
+				return true;
+			} catch (Exception e) {
+				e.printStackTrace();
+				return true;
 			}
 
+		} else {
+			return false;
 		}
-		return false;
 	}
-
-	public Long getSlotDefId() {
-		return this.slotDefId;
-	}
-
-	public Long getSlotInstId() {
-		return this.slotInstId;
-	}
-
-	public void setSlotDefId(Long slotDefId) {
-		this.slotDefId = slotDefId;
-	}
-
-	public void setSlotInstId(Long slotInstId) {
-		this.slotInstId = slotInstId;
-	}
-
 }
