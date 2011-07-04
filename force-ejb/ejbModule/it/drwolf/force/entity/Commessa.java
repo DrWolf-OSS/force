e10be6250b5c378323619c04ec81a5df904ea07d
Stefano Raffini <stefanoraffini@drwolf.it>
2011-07-04 16:36:29 +0200

    Corretto il nome della tabella nell'entity

--- force-ejb/ejbModule/it/drwolf/force/entity/Commessa.java
+++ force-ejb/ejbModule/it/drwolf/force/entity/Commessa.java
@@ -14,7 +14,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "Comessa")
+@Table(name = "Commessa")
 public class Commessa implements Serializable {
 
 	/**
