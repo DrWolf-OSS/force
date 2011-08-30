03990f243e6d8dfd506f08ddc2d9050227538a6a
Stefano Raffini <stefanoraffini@drwolf.it>
2011-08-30 10:46:51 +0200

    Completato lo sviluppo del box i miei documenti in scadenza nel control panel. Ora vengono visualizzati il primi due documenti in scadenza
    Signed-off-by: Stefano Raffini <stefanoraffini@drwolf.it>

--- force-ejb/ejbModule/it/drwolf/force/session/ControlPanelBean.java
+++ force-ejb/ejbModule/it/drwolf/force/session/ControlPanelBean.java
@@ -3,16 +3,22 @@ package it.drwolf.force.session;
 import it.drwolf.force.entity.Gara;
 import it.drwolf.slot.alfresco.AlfrescoUserIdentity;
 import it.drwolf.slot.alfresco.AlfrescoWrapper;
+import it.drwolf.slot.alfresco.custom.model.Property;
+import it.drwolf.slot.application.CustomModelController;
 import it.drwolf.slot.entity.DocInst;
 import it.drwolf.slot.entity.DocInstCollection;
 import it.drwolf.slot.entity.SlotInst;
 import it.drwolf.slot.pagebeans.SlotInstEditBean;
+import it.drwolf.slot.pagebeans.support.FileContainer;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 
+import org.alfresco.cmis.client.AlfrescoDocument;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Create;
 import org.jboss.seam.annotations.In;
@@ -38,12 +44,27 @@ public class ControlPanelBean {
 	@In(create = true)
 	private AlfrescoWrapper alfrescoWrapper;
 
+	@In(create = true)
+	private CustomModelController customModelController;
+
+	private ArrayList<DocInstCollection> inScadenza;
+
+	private HashMap<Long, List<FileContainer>> inScadenzaFC;
+
 	public List<DocInstCollection> getDocInstCollections() {
 		for (DocInstCollection element : this.slotInstEditBean
 				.getDocInstCollections()) {
 			System.out.println(element);
 		}
 		return this.slotInstEditBean.getDocInstCollections();
+	}
+
+	public ArrayList<DocInstCollection> getInScadenza() {
+		return this.inScadenza;
+	}
+
+	public HashMap<Long, List<FileContainer>> getInScadenzaFC() {
+		return this.inScadenzaFC;
 	}
 
 	public List<Gara> getSelectedGare() {
@@ -76,16 +97,43 @@ public class ControlPanelBean {
 				.getDocumentIdsInFolderByAspect("slot:expirable",
 						this.userSession.getPrimarySlotFolder().getId(),
 						"slot:expirationDate", 2);
+		this.inScadenza = new ArrayList<DocInstCollection>();
+		this.inScadenzaFC = new HashMap<Long, List<FileContainer>>();
+		HashSet<Property> properties = new HashSet<Property>();
+		properties.add(this.customModelController
+				.getProperty("slot:expirationDate"));
 		for (DocInstCollection docs : this.slotInstEditBean
 				.getDocInstCollections()) {
+			DocInstCollection docInst = new DocInstCollection(
+					docs.getSlotInst(), docs.getDocDefCollection());
+			List<FileContainer> fileContainers = new ArrayList<FileContainer>();
+			boolean added = false;
 			for (DocInst doc : docs.getDocInsts()) {
 				for (String id : ids) {
 					if (doc.getNodeRef().equals(id)) {
-						// documento prossimo alla scadenza
-						System.out.println(doc);
+						added = true;
+						docInst.getDocInsts().add(doc);
+						AlfrescoDocument ad = (AlfrescoDocument) this.alfrescoUserIdentity
+								.getSession().getObject(doc.getNodeRef());
+						FileContainer fc = new FileContainer(ad, properties,
+								false);
+						fileContainers.add(fc);
 					}
 				}
 			}
+			if (added) {
+				this.inScadenza.add(docInst);
+				this.inScadenzaFC.put(docInst.getDocDefCollection().getId(),
+						fileContainers);
+			}
 		}
+	}
+
+	public void setInScadenza(ArrayList<DocInstCollection> inscadenza) {
+		this.inScadenza = inscadenza;
+	}
+
+	public void setInScadenzaFC(HashMap<Long, List<FileContainer>> inScadenzaFC) {
+		this.inScadenzaFC = inScadenzaFC;
 	}
 }
