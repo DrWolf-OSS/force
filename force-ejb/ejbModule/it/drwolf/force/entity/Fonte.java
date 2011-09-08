18e8ae56bac5ca8f70da99713c3d6aaa8089c644
Stefano Raffini <stefanoraffini@drwolf.it>
2011-09-09 00:23:05 +0200

    Aggiunta la entity Fonte e l'enum associato TipoFonte
    Signed-off-by: Stefano Raffini <stefanoraffini@drwolf.it>

---
+++ force-ejb/ejbModule/it/drwolf/force/entity/Fonte.java
@@ -0,0 +1,65 @@
+package it.drwolf.force.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Fonte")
+public class Fonte implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4815586393632496376L;
+
+	private Integer id;
+
+	private String nome;
+
+	private String url;
+
+	private String tipo;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Integer getId() {
+		return this.id;
+	}
+
+	@Column
+	public String getNome() {
+		return this.nome;
+	}
+
+	@Column
+	public String getTipo() {
+		return this.tipo;
+	}
+
+	@Column
+	public String getUrl() {
+		return this.url;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
---
+++ force-ejb/ejbModule/it/drwolf/force/enums/TipoFonte.java
@@ -0,0 +1,20 @@
+package it.drwolf.force.enums;
+
+public enum TipoFonte {
+
+	START("S.T.A.R.T."), SITAT("S.I.T.A.T");
+
+	private String testo;
+
+	private TipoFonte(String testo) {
+		this.testo = testo;
+	}
+
+	public String getNome() {
+		return this.name();
+	}
+
+	public String getTesto() {
+		return this.testo;
+	}
+}
