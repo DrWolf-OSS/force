6dbfdb01e05a99620383823cc922923ab1ea9d36
Stefano Raffini <stefanoraffini@drwolf.it>
2011-09-09 00:24:05 +0200

    Ora funziona anche con l'entityManager. Prendi gli url dalle fonti e li inserisci nel db
    Signed-off-by: Stefano Raffini <stefanoraffini@drwolf.it>

--- force-ejb/ejbModule/it/drwolf/force/timer/Heartbeat.java
+++ force-ejb/ejbModule/it/drwolf/force/timer/Heartbeat.java
@@ -1,11 +1,20 @@
 package it.drwolf.force.timer;
 
+import it.drwolf.force.entity.Fonte;
+import it.drwolf.force.entity.Gara;
+import it.drwolf.force.enums.TipoGara;
+
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import org.jboss.seam.Component;
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Transactional;
@@ -35,34 +44,64 @@ public class Heartbeat {
 		QuartzTriggerHandle handle = new QuartzTriggerHandle(
 				"Fetcher dei fedd del sistema START");
 
-		URL url = null;
-		try {
-			url = new URL(
-					"http://start.e.toscana.it/regione-toscana/pleiade/XML/regione-toscana.xml");
-		} catch (MalformedURLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		EntityManager entityManager = null;
+
+		while (entityManager == null) {
+			try {
+				entityManager = (EntityManager) Component
+						.getInstance("entityManager");
+				System.out.println("---> Sleep!");
+				Thread.sleep(2000);
+			} catch (Exception e) {
+
+			}
 		}
 
-		ClassLoader c1 = Heartbeat.class.getClassLoader();
-		Thread.currentThread().setContextClassLoader(c1);
-		SyndFeed feed = null;
-		try {
-			feed = new SyndFeedInput().build(new XmlReader(url));
-			Iterator i = feed.getEntries().iterator();
-			while (i.hasNext()) {
-				SyndEntry post = (SyndEntry) i.next();
-				System.out.println(post.getTitle());
+		ArrayList<Fonte> listaFonti = (ArrayList<Fonte>) entityManager
+				.createQuery("from Fonte").getResultList();
+		for (Fonte fonte : listaFonti) {
+			URL url = null;
+			try {
+				url = new URL(fonte.getUrl());
+			} catch (MalformedURLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (FeedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+			ClassLoader c1 = Heartbeat.class.getClassLoader();
+			Thread.currentThread().setContextClassLoader(c1);
+			SyndFeed feed = null;
+			try {
+				feed = new SyndFeedInput().build(new XmlReader(url));
+				Iterator i = feed.getEntries().iterator();
+				while (i.hasNext()) {
+					SyndEntry post = (SyndEntry) i.next();
+					List results = entityManager
+							.createQuery("from Gara where oggetto = :oggetto")
+							.setParameter("oggetto", post.getTitle())
+							.getResultList();
+					if (results.size() == 0) {
+						// nuova Gara. La inserisco
+						Gara gara = new Gara(post.getTitle(), post.getLink(),
+								TipoGara.NUOVA.getNome(), fonte);
+						entityManager.persist(gara);
+						// a questo punto devo andare a prenderemi dal link i
+						// valori
+						// della gara che sono nella pagina del dettaglio
+
+					}
+				}
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (FeedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 		}
 
 		return handle;
