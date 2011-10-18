7943f3aa590b94a874e2bf5843a8aeeefb72e851
Stefano Raffini <stefanoraffini@drwolf.it>
2011-10-18 12:05:48 +0200

    Creato il parser per le gare publbicate su avcp
    Signed-off-by: Stefano Raffini <stefanoraffini@drwolf.it>

---
+++ force-ejb/ejbModule/it/drwolf/force/utils/AvcpFeedParser.java
@@ -0,0 +1,30 @@
+package it.drwolf.force.utils;
+
+import java.util.Date;
+import java.util.List;
+
+import it.drwolf.force.interfaces.GaraFeedParserIF;
+
+public class AvcpFeedParser implements GaraFeedParserIF {
+
+	public List<String> getCategorie() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Date getDataFine() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Date getDataInizio() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void parse(String url) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
